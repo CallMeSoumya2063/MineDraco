c550f80ab6fafcd6f2d6cf6276d307b180bfa445
CallMeSoumya2063 <81620896+CallMeSoumya2063@users.noreply.github.com>
2024-06-04 08:10:16 +0530

    Update injector.sh for v0.1.5
    Apart from updating the script to use release v0.1.5 of draco injector, here are more changes:
    
    Removed apt update, apt upgrade, asking user about architecture
    
    Added x86_64 support, check for curl package installation

--- injector.sh
+++ injector.sh
@@ -11,80 +11,25 @@ fi
 # Determine device architecture
 arch=$(uname -m)
 
-# Allow user to manually set architecture for 30 seconds
-echo -e "\nDetected architecture: '$arch'. You have 30 seconds to use architecture other than '$arch' (aarch64 or arm), or press ENTER to continue with detected architecture.\n"
-while true; do
-    read -t 30 -p "Enter architecture or press ENTER: " manual_arch
-
-    if [[ -z "$manual_arch" ]]; then
-        echo -e "\n\nNo architecture entered, continuing with detected architecture.\n"
-        break
-    elif [[ "$manual_arch" == "aarch64" || "$manual_arch" == "arm" ]]; then
-        echo -e "\nWarning: Manually changing architecture can lead to compatibility issues."
-        arch=$manual_arch
-        echo -e "\nArchitecture manually set to $arch"
-        break
-    else
-        echo -e "Invalid architecture entered, try again.\n"
-    fi
-done
-
 # Define download URLs based on architecture
 case "$arch" in
     aarch64)
-        injector_url="https://github.com/mcbegamerxx954/draco-injector/releases/download/v0.1.4/injector-aarch64-linux-android.tar.gz"
-        injector_file="injector-aarch64-linux-android.tar.gz"
+        injector_url="https://github.com/mcbegamerxx954/draco-injector/releases/download/v0.1.5/injector-aarch64-linux-android.tar.gz"
+        injector_file="v0.1.5_injector-aarch64-linux-android.tar.gz"
         ;;
     armv7l | arm)
-        injector_url="https://github.com/mcbegamerxx954/draco-injector/releases/download/v0.1.4/injector-armv7-linux-androideabi.tar.gz"
-        injector_file="injector-armv7-linux-androideabi.tar.gz"
+        injector_url="https://github.com/mcbegamerxx954/draco-injector/releases/download/v0.1.5/injector-armv7-linux-androideabi.tar.gz"
+        injector_file="v0.1.5_injector-armv7-linux-androideabi.tar.gz"
+        ;;
+    x86_64)
+        injector_url="https://github.com/mcbegamerxx954/draco-injector/releases/download/v0.1.5/injector-x86_64-unknown-linux-gnu.tar.gz"
+        injector_file="v0.1.5_injector-x86_64-unknown-linux-gnu.tar.gz"
         ;;
     *)
         echo "Unsupported architecture: $arch"
         exit 1
         ;;
 esac
-
-# Function to ask user about architecture
-ask_user_about_architecture() {
-    while true; do
-        read -p "Are you sure that the APK file you are going to patch supports '$arch'? (reply yes or no) " consent
-        case "$consent" in
-            [Yy][Ee][Ss]|[Yy])
-                echo -e "Okay, continuing process...\n"
-                break
-                ;;
-            [Nn][Oo]|[Nn])
-                while true; do
-                    echo -e "\n"
-                    read -p "Same architecture on device and in APK file gives best compatibility and performance. Are you sure you still wish to continue? (reply yes or no) " confirm
-                    case "$confirm" in
-                        [Yy][Ee][Ss]|[Yy])
-                            echo -e "Okay, forcibly continuing process...\n"
-                            break 2
-                            ;;
-                        [Nn][Oo]|[Nn])
-                            echo -e "\nStopping process...\n"
-                            exit 1
-                            ;;
-                        * )
-                            echo -e "\nPlease answer yes or no."
-                            ;;
-                    esac
-                done
-                ;;
-            * )
-                echo -e "\nPlease answer yes or no.\n"
-                ;;
-        esac
-    done
-}
-
-# Call the function to ask user about architecture
-ask_user_about_architecture
-
-# Update and upgrade packages
-apt update -y && apt upgrade -y
 
 echo -e "\nSearching for all Minecraft APK files in storage/emulated/0/Download, this may take some time...\n\n"
 
@@ -117,6 +62,20 @@ read -p "App Name: " app
 read -p "Package Name (enter a valid name): " pack
 read -p "Output File Name (include .apk): " out
 
+package="curl"
+
+# Check if curl is installed, install if necessary
+if dpkg -l | grep -q "^ii  $package "; then
+    echo -e "Package '$package' is installed. Continuing process...\n"
+else
+    echo -e "Package '$package' is not installed. Installing '$package' before continuing.\n"
+    if ! apt install curl -y; then
+        echo -e "Failed to install package '$package'\n"
+        exit 1
+    fi
+    echo -e "Package '$package' has been installed successfully. Continuing process...\n"
+fi
+
 # Check if the injector file already exists then download
 if [ -f "$injector_file" ]; then
     echo -e "Injector file already exists, skipping download.\n"
@@ -139,5 +98,5 @@ fi
 mv "$out" /storage/emulated/0/Download
 
 # Done, exit
-echo -e "\nProcess completed successfully.\n"
+echo -e "\nPatched Minecraft APK created successfully in Download folder.\n"
 exit 0
